5d7bd8150fbe6713aaec8eb317dc935a5ccc8492
Den Gl <dengl@MacBook-Air-Den.local>
2022-07-17 23:27:01 +0300

    Add new pages

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,17 @@
-new AirDatepicker('.earn__datepicker', {
-    inline: true
-})
-
 $( document ).ready(function() {
+
+    new AirDatepicker('.earn__datepicker', {
+        inline: true
+    });
+
+    var form_datepickers = document.querySelectorAll('.form__datepicker');
+
+    form_datepickers.forEach((form_datepicker) => {
+        new AirDatepicker(form_datepicker, {
+            range: true,
+            multipleDatesSeparator: ' - '
+        });
+    });
 
     $('[data-control]').on('click', function() {
         let target = $(this).data('control');
@@ -12,6 +21,10 @@ $( document ).ready(function() {
     $('[data-toggle]').on('click', function() {
         $(this).toggleClass('active');
     });
+
+    $('[data-tab]').on('click', function() {
+        $(this).addClass('active').siblings().removeClass('active');
+    });
     
     $('[data-nav]').on('click', function() {
         $(this).addClass('active').siblings().removeClass('active');
@@ -19,6 +32,10 @@ $( document ).ready(function() {
         let target = $(this).data('nav');
         $('.' + target).removeClass('active');
         $('.' + target).eq(index).addClass('active');
+    });
+
+    $('[data-unscroll]').on('click', function() {
+        $('body').toggleClass('unscroll');
     });
 
     $('[data-amount]').on('click', function() {
@@ -79,6 +96,33 @@ $( document ).ready(function() {
         $('.list__rows').html(result);
     });
 
+    $('.modal').on('mousedown', function(e) {
+        if ( e.target !== this ) return;
+        $(this).removeClass('active');
+    });
+
+    $('.card__slider').slick({
+        infinite: false,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 2.5,
+                    arrows: false
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1.5,
+                    arrows: false
+                }
+            }
+        ]
+    });
+
     $('.project__gallery').slick({
         infinite: true,
         slidesToShow: 1,
@@ -93,5 +137,113 @@ $( document ).ready(function() {
             }
         ]
     });
+
+    $('.roadmap__slider').slick({
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        dots: false,
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1,
+                    arrows: false
+                }
+            }
+        ]
+    });
+
+    $('label[for=file]').each(function() {
+        let input    = $(this).find('input[type=file]');
+        let fileName = $(this).find('span');
+
+        input.on('change', function(e) {
+            e.target.closest('label').classList.add('uploaded');
+            let inputFile = e.target.files[0].name;
+            fileName.text(inputFile);
+        })
+    });
+
+    /* Custom select */
+
+    var x, i, j, l, ll, selElmnt, a, b, c;
+    /*look for any elements with the class "custom-select":*/
+    x = document.getElementsByClassName("select__custom");
+    l = x.length;
+    for (i = 0; i < l; i++) {
+    selElmnt = x[i].getElementsByTagName("select")[0];
+    ll = selElmnt.length;
+    /*for each element, create a new DIV that will act as the selected item:*/
+    a = document.createElement("DIV");
+    a.setAttribute("class", "select-selected");
+    a.innerHTML = selElmnt.options[selElmnt.selectedIndex].innerHTML;
+    x[i].appendChild(a);
+    /*for each element, create a new DIV that will contain the option list:*/
+    b = document.createElement("DIV");
+    b.setAttribute("class", "select-items select-hide");
+    for (j = 1; j < ll; j++) {
+        /*for each option in the original select element,
+        create a new DIV that will act as an option item:*/
+        c = document.createElement("DIV");
+        c.innerHTML = selElmnt.options[j].innerHTML;
+        c.addEventListener("click", function(e) {
+            /*when an item is clicked, update the original select box,
+            and the selected item:*/
+            var y, i, k, s, h, sl, yl;
+            s = this.parentNode.parentNode.getElementsByTagName("select")[0];
+            sl = s.length;
+            h = this.parentNode.previousSibling;
+            for (i = 0; i < sl; i++) {
+            if (s.options[i].innerHTML == this.innerHTML) {
+                s.selectedIndex = i;
+                h.innerHTML = this.innerHTML;
+                y = this.parentNode.getElementsByClassName("same-as-selected");
+                yl = y.length;
+                for (k = 0; k < yl; k++) {
+                y[k].removeAttribute("class");
+                }
+                this.setAttribute("class", "same-as-selected");
+                break;
+            }
+            }
+            h.click();
+        });
+        b.appendChild(c);
+    }
+    x[i].appendChild(b);
+    a.addEventListener("click", function(e) {
+        /*when the select box is clicked, close any other select boxes,
+        and open/close the current select box:*/
+        e.stopPropagation();
+        closeAllSelect(this);
+        this.nextSibling.classList.toggle("select-hide");
+        this.classList.toggle("select-arrow-active");
+        });
+    }
+    function closeAllSelect(elmnt) {
+    /*a function that will close all select boxes in the document,
+    except the current select box:*/
+    var x, y, i, xl, yl, arrNo = [];
+    x = document.getElementsByClassName("select-items");
+    y = document.getElementsByClassName("select-selected");
+    xl = x.length;
+    yl = y.length;
+    for (i = 0; i < yl; i++) {
+        if (elmnt == y[i]) {
+        arrNo.push(i)
+        } else {
+        y[i].classList.remove("select-arrow-active");
+        }
+    }
+    for (i = 0; i < xl; i++) {
+        if (arrNo.indexOf(i)) {
+        x[i].classList.add("select-hide");
+        }
+    }
+    }
+    /*if the user clicks anywhere outside the select box,
+    then close all select boxes:*/
+    document.addEventListener("click", closeAllSelect);
 
 });
